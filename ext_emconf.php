ea31cf11f2bc23f1b6a7541ec729df1948363aab
Andreas Pfeiffer <andreas.pfeiffer@e-pixler.com>
2021-09-02 11:16:10 +0200

    [TASK] Refactor code in provider extension; maximize possible TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF['slub_profile_events'] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.4.99'
+            'typo3' => '11.0.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
