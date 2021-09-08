60768b505cb37734f0f976dced627f5b5643b0e7
Andreas Pfeiffer <andreas.pfeiffer@e-pixler.com>
2021-09-08 17:31:51 +0200

    [TASK] Update to TYPO3 v11.4

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,46 +1,25 @@
 <?php
 
+use Slub\SlubProfileEvents\Controller\EventController;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3') || die();
 
 // Add tsconfig page
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+ExtensionManagementUtility::addPageTSConfig(
     '@import "EXT:slub_profile_events/Configuration/TsConfig/Page.tsconfig"'
 );
 
 // Configure plugin - event list
-TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+ExtensionUtility::configurePlugin(
     'SlubProfileEvents',
     'EventList',
     [
-        Slub\SlubProfileEvents\Controller\EventController::class => 'list'
+        EventController::class => 'list'
     ],
     [
-        Slub\SlubProfileEvents\Controller\EventController::class => 'list'
+        EventController::class => 'list'
     ],
-    TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-);
-
-/**
- * @todo Refactor icon configuration
- *
- * Still necessary to test the icons
- * In master or v11.4 (2021-09-07) the icons should work by the Icons.php in Configuration
- * It is already prepared
- * Remove the following code when the other works
- */
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Imaging\IconRegistry::class
-);
-
-$iconRegistry->registerIcon(
-    'slubprofileevents-wizard-eventlist',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:slub_profile_events/Resources/Public/Icons/Wizard/eventlist.svg']
-);
-
-$iconRegistry->registerIcon(
-    'slubprofileevents-overlay-extension',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:slub_profile_events/Resources/Public/Icons/Overlay/extension.svg']
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
